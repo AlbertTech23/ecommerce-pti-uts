c770e22fb5b6eff997c47cdb5a0b9e4baa80e262
AlbertTech23 <alberttirtokusumo@gmail.com>
2023-10-21 22:25:08 +0700

    jadiin navbar, products logic, responsive, fitur++

---
+++ products.js
@@ -0,0 +1,16 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "SONY FX3 Full-frame Cinema Line Body Only",
+    price: 3999.99,
+    productImage: "./assets/products/fx3.png",
+    discountedPrice: 3599.99,
+  },
+  {
+    id: 2,
+    productName: "Hollyland Cosmo C1 SDI/HDMI Wireless",
+    price: 784.99,
+    productImage: "./assets/products/cosmo-c1.png",
+    discountedPrice: 699.99,
+  },
+];
